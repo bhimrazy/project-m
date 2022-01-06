0ea4c2c7c444f0d2602dd7a1db033accb0a3ae9b
BHIMRAJ YADAV <bhimrajyadav977@gmail.com>
2022-01-06 17:54:09 +0000

    Added a slug page

--- components/section.js
+++ components/section.js
@@ -1,42 +1,45 @@
 import React from "react";
 import Image from "next/image";
+import Link from "next/link";
+
 export default function Section({ title, content }) {
-    // const image_url = 'https://static01.nyt.com/images/2022/01/06/us/politics/06dc-assess/merlin_199965939_42d55dc7-8497-439d-8f6c-5dcd4c21038b-threeByTwoMediumAt2X.jpg?format=pjpg&quality=75&auto=webp&disable=upscale'
     return (
         <section className="max-w-7xl container mx-auto px-8 py-4 sm:px-28">
             <h3 className="text-3xl font-extrabold text-gray-900 py-7">{title}</h3>
             <div className="col-span-3 grid sm:grid-cols-2 lg:grid-cols-3  gap-6 sm:gap-y-8 lg:gap-x-8">
                 {
                     content && content.map((item, index) => (
-                        <a href="/" key={index}>
-                            <article className="flex flex-col">
-                                <Image
-                                    className="h-48 w-96 bg-cover bg-center object-cover rounded-lg shadow-lg"
-                                    src={item?.img_url}
-                                    alt="Falcon"
-                                    width={1920}
-                                    height={1280}
-                                    placeholder="blur"
-                                    blurDataURL={item?.img_url}
-                                />
-                                <div className="flex flex-col space-y-1">
-                                    <p className="text-cool-500 text-sm tracking-wide pt-4">
-                                        Jan 18,2021
-                                    </p>
-                                    <h2 className="text-2xl font-bold tracking-tight text-cool-800">
-                                        {item?.title}
-                                    </h2>
-                                    <p className="text-cool-600 text-sm leading-relaxed">
-                                        {item?.description}
-                                    </p>
-                                    <div>
-                                        <button className="hidden px-6 py-3 rounded-md left-0 text-cool-100 text-sm font-semibold bg-cool-900 hover:bg-cool-800 focus:outline-none">
-                                            Read More.
-                                        </button>
+                        <Link key={index} href="/[category]/[slug]" as={`/${title.toLowerCase()}/${item?.slug}`}>
+                            <a>
+                                <article className="flex flex-col">
+                                    <Image
+                                        className="h-48 w-96 bg-cover bg-center object-cover rounded-lg shadow-lg"
+                                        src={item?.img_url}
+                                        alt="Falcon"
+                                        width={1920}
+                                        height={1280}
+                                        placeholder="blur"
+                                        blurDataURL={item?.img_url}
+                                    />
+                                    <div className="flex flex-col space-y-1">
+                                        <p className="text-cool-500 text-sm tracking-wide pt-4">
+                                            Jan 18,2021
+                                        </p>
+                                        <h2 className="text-2xl font-bold tracking-tight text-cool-800">
+                                            {item?.title}
+                                        </h2>
+                                        <p className="text-cool-600 text-sm leading-relaxed">
+                                            {item?.description}
+                                        </p>
+                                        <div>
+                                            <button className="hidden px-6 py-3 rounded-md left-0 text-cool-100 text-sm font-semibold bg-cool-900 hover:bg-cool-800 focus:outline-none">
+                                                Read More.
+                                            </button>
+                                        </div>
                                     </div>
-                                </div>
-                            </article>
-                        </a>
+                                </article>
+                            </a>
+                        </Link>
                     ))
                 }
             </div>
---
+++ pages/[category]/[slug].js
@@ -0,0 +1,65 @@
+import React from 'react'
+import Image from "next/image";
+
+export default function Index({ post }) {
+    return (
+        <div className="flex flex-col max-w-5xl mx-auto py-20">
+            <article className="flex flex-col">
+                <Image
+                    className="bg-cover bg-center object-cover rounded-lg shadow-lg"
+                    src={post?.img_url}
+                    alt="Falcon"
+                    width={1920}
+                    height={1280}
+                    placeholder="blur"
+                    blurDataURL={post?.img_url}
+                />
+                <div className="flex flex-col space-y-1">
+                    <p className="text-cool-500 text-sm tracking-wide pt-4">
+                        Jan 18,2021
+                    </p>
+                    <h2 className="text-2xl font-bold tracking-tight text-cool-800">
+                        {post?.title}
+                    </h2>
+                    <p className="text-cool-600 text-sm leading-relaxed">
+                        {post?.description}
+                    </p>
+                    <div>
+                        <button className="hidden px-6 py-3 rounded-md left-0 text-cool-100 text-sm font-semibold bg-cool-900 hover:bg-cool-800 focus:outline-none">
+                            Read More.
+                        </button>
+                    </div>
+                </div>
+            </article>
+        </div>
+    )
+}
+
+// export async function getStaticPaths() {
+
+//     const res = await fetch('https://3000-tan-panther-z3e1aw7u.ws-us25.gitpod.io/api')
+//     const posts = await res.json()
+
+//     const paths = []
+//     posts?.data.map(cat => {
+//         return cat.content.map(post => {
+//             paths.push({
+//                 params: {
+//                     category: cat.category.toLowerCase(),
+//                     slug: post.slug
+//                 }
+//             })
+//         })
+//     })
+//     console.log(paths)
+//     return { paths, fallback: false }
+// }
+
+export async function getServerSideProps({ params }) {
+    const category = params.category;
+    const slug = params.slug;
+    const res = await fetch('https://3000-tan-panther-z3e1aw7u.ws-us25.gitpod.io/api')
+    const posts = await res.json()
+    const post = posts?.data?.find(cat => cat?.category.toLowerCase() === category)?.content?.find(post => post?.slug === slug)
+    return { props: { post } }
+}
